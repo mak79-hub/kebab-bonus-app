1256f460ebc17a94ed32818b71a4afe6602c3092
mak79-hub <kirec.m@yahoo.com>
2026-07-19 00:56:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
+const CACHE_NAME = "kebab-bonus-v1";
 
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+    // Im ersten Schritt keine Offline-Caches.
+});
